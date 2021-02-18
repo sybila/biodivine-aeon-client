6776ce2be3fc752bf9ca140494073ea61714de70
Samuel Pastva <daemontus@gmail.com>
2021-02-18 23:35:55 +0100

    Add sink stability analysis results.

--- script/treeExplorerMain.js
+++ script/treeExplorerMain.js
@@ -93,6 +93,15 @@ function openTreeWitness() {
     window.open(url + '?engine=' + encodeURI(ComputeEngine.getAddress()) + "&tree_witness="+ encodeURI(node));
 }
 
+function openStabilityWitness(variable, behaviour) {
+	let node = CytoscapeEditor.getSelectedNodeId();
+	if (node === undefined) {
+		return;
+	}
+	const url = window.location.pathname.replace("tree_explorer.html", "index.html");
+    window.open(url + '?engine=' + encodeURI(ComputeEngine.getAddress()) + "&tree_witness="+ encodeURI(node) + "&variable=" + encodeURI(variable) + "&value=" + encodeURI(behaviour));
+}
+
 /* Open attractors for the currently selected tree node. */
 function openTreeAttractor() {
 	let node = CytoscapeEditor.getSelectedNodeId();
@@ -101,6 +110,15 @@ function openTreeAttractor() {
 	}
 	const url = window.location.pathname.replace("tree_explorer.html", "explorer.html");
     window.open(url + '?engine=' + encodeURI(ComputeEngine.getAddress()) + "&tree_witness="+ encodeURI(node));
+}
+
+function openStabilityAttractor(variable, behaviour) {
+	let node = CytoscapeEditor.getSelectedNodeId();
+	if (node === undefined) {
+		return;
+	}
+	const url = window.location.pathname.replace("tree_explorer.html", "explorer.html");
+    window.open(url + '?engine=' + encodeURI(ComputeEngine.getAddress()) + "&tree_witness="+ encodeURI(node) + "&variable=" + encodeURI(variable) + "&value=" + encodeURI(behaviour));
 }
 
 // Used to initialize a stability analysis button in the detail panels.
@@ -114,21 +132,54 @@ function initStabilityButton(id, button, container) {
                 console.log(e);
                 alert("Cannot load stability data.");                   
             } else {
-                button.classList.add("gone");                
+                button.classList.add("gone");             
                 let alwaysTrue = "<div><b>Always true:</b> <span class='green'>"+r["always_true"].join("; ") + "</span><div>";
                 if(r["always_true"].length == 0) { alwaysTrue = ""; }
                 let alwaysFalse = "<div><b>Always false:</b> <span class='red'>"+r["always_false"].join("; ") + "</span><div>";
                 if(r["always_false"].length == 0) { alwaysFalse = ""; }
-                let constant = "<div><b>Constant:</b> "+r["constant"].join("; ") + "<div>";
+                let constant = "<div><b>Constant for parametrisation:</b> "+r["constant"].join("; ") + "<div>";
                 if(r["constant"].length == 0) { constant = ""; }
-                let sinkCount = "<div><b>Unique sinks:</b> "+r["sink_count"] + "<div>";
-                if(r["sink_count"] == "0") { sinkCount = ""; }
-                let content = alwaysTrue + alwaysFalse + constant + sinkCount;
+                let content = alwaysTrue + alwaysFalse + constant;
                 if(content.length == 0) {
                 	content = "No stable variables found."
+                } else {
+                	content = "<h4>All attractors:</h4>" + content;
+                }
+
+                if('var_stability' in r) {
+                	let var_analysis = "";
+                	for(v of r['var_stability']) {
+                		if('constant' in v) {
+                			if(v['constant']) {
+                				var_analysis += "<div><b>" + v['name'] + "</b>:</br> <span class='green'> - always true</span></div>"
+                			} else {
+                				var_analysis += "<div><b>" + v['name'] + "</b>:</br> <span class='red'> - always false</span></div>"
+                			}
+                		} else {
+                			let distribution = "";
+                			if(v['only_true'] > 0.0) {
+                				distribution += " - With <span class='green'>true</span>: "+v['only_true'] + getWitnessPanelForVariable(v['name'], 'true')+"</br>";
+                			}
+                			if(v['only_false'] > 0.0) {
+                				distribution += " - With <span class='red'>false</span>: "+v['only_false'] + getWitnessPanelForVariable(v['name'], 'false')+"</br>";
+                			}
+                			if(v['mixed'] > 0.0) {
+                				distribution += " - With <b>both</b>: "+v['mixed'] + getWitnessPanelForVariable(v['name'], 'mixed')+"</br>";
+                			}
+                			var_analysis += "<div><b>" + v['name'] + "</b>:</br>" + distribution + "</div>";
+
+                		}
+                	}
+                	if(var_analysis.length > 0) {
+                		content += "<h4>Sink attractors:</h4>" + var_analysis;
+                	}
                 }
                 container.innerHTML = content;
             }
         })
     }
+}
+
+function getWitnessPanelForVariable(variable, behaviour) {
+	return "<span style='float: right;'><span class='inline-button' onclick='openStabilityWitness(\""+variable+"\",\""+behaviour+"\");'>Witness</span> | <span class='inline-button' onclick='openStabilityAttractor(\""+variable+"\",\""+behaviour+"\");'>Attractor</span></span>";
 }
